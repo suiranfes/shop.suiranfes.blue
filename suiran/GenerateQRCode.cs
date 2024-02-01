7f672ad1d4f2796fefe8cb6da671f4aa70c6159b
mint73 <ny.snoopy.1164@gmail.com>
2024-02-01 12:35:19 +0900

    change: syntax of "new"

--- suiran/GenerateQRCode.cs
+++ suiran/GenerateQRCode.cs
@@ -5,9 +5,9 @@ public static class GenerateQRCode
 {
     public static string GenerateQR(string input)
     {
-        QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        QRCodeGenerator qrGenerator = new();
         QRCodeData qrCodeData = qrGenerator.CreateQrCode(input, QRCodeGenerator.ECCLevel.H);
-        PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
+        PngByteQRCode qrCode = new(qrCodeData);
         var qrCodeImage = qrCode.GetGraphic(20);
         var qrString = "data:image/png;base64," + Convert.ToBase64String(qrCodeImage.ToArray());
         return qrString;
