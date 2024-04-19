34b3adc73f6ec55b3f16237e8f920bb8b1acf517
mint73 <ny.snoopy.1164@gmail.com>
2024-04-20 00:25:12 +0900

    change: ecc-level of the qrcode

--- suiran/GenerateQRCode.cs
+++ suiran/GenerateQRCode.cs
@@ -6,7 +6,7 @@ public static class GenerateQRCode
     public static string GenerateQR(string input)
     {
         QRCodeGenerator qrGenerator = new();
-        QRCodeData qrCodeData = qrGenerator.CreateQrCode(input, QRCodeGenerator.ECCLevel.H);
+        QRCodeData qrCodeData = qrGenerator.CreateQrCode(input, QRCodeGenerator.ECCLevel.M);
         PngByteQRCode qrCode = new(qrCodeData);
         var qrCodeImage = qrCode.GetGraphic(20);
         var qrString = "data:image/png;base64," + Convert.ToBase64String(qrCodeImage.ToArray());
