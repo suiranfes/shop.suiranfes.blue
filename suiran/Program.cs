a97260ec43b3b7fc61a6e5e7158a85b631f5c806
mint73 <ny.snoopy.1164@gmail.com>
2024-05-15 09:44:52 +0900

    Compression (brotli) #33

--- suiran/Program.cs
+++ suiran/Program.cs
@@ -1,10 +1,14 @@
+using System.IO.Compression;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
 using suiran;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.Configure<BrotliCompressionProviderOptions>(options => options.Level = CompressionLevel.SmallestSize);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
